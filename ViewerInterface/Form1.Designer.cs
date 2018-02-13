fd95287ab815f1e18a79ed1c603f58c830b82c9a
Progman-nkk <pneucolas@gmail.com>
2018-02-12 20:20:47 -0500

    still need to add new viewer label

---
+++ ViewerInterface/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ViewerInterface
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_logger = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_logger
+            // 
+            this.txt_logger.BackColor = System.Drawing.Color.Black;
+            this.txt_logger.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_logger.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_logger.ForeColor = System.Drawing.Color.White;
+            this.txt_logger.Location = new System.Drawing.Point(0, 0);
+            this.txt_logger.Multiline = true;
+            this.txt_logger.Name = "txt_logger";
+            this.txt_logger.ReadOnly = true;
+            this.txt_logger.Size = new System.Drawing.Size(223, 107);
+            this.txt_logger.TabIndex = 2;
+            this.txt_logger.Text = "Brea\r\nCalifornia\r\nUnited States\r\n1515826863.client";
+            // 
+            // MainWindow
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(223, 107);
+            this.Controls.Add(this.txt_logger);
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "compusa.live";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_logger;
+        
+    }
+}
+
---
+++ ViewerInterface/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System.IO;
+using System.Threading;
+
+namespace ViewerInterface
+{
+    public partial class MainWindow : Form
+    {
+        SftpClient compusaServer;
+        GlobalDataBase envMonitor;
+        BackgroundWorker fisherMan;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        
+        public void hireFisherMan()
+        {
+            if(fisherMan == null)
+            {
+                fisherMan = new BackgroundWorker();
+                fisherMan.WorkerReportsProgress = true;
+                fisherMan.DoWork += fisherMan_DoWork;
+                fisherMan.RunWorkerCompleted += fisherMan_RunWorkerCompleted;
+                fisherMan.RunWorkerAsync();
+            }
+        } 
+        private void fisherMan_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            monitorSharedMemory();
+            
+            Thread.Sleep(500);
+        }
+        private void fisherMan_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            BackgroundWorker worker = (BackgroundWorker)sender;
+            if (envMonitor.NewContent)
+            {
+                updateGUI();
+                envMonitor.NewContent = false;
+            }
+            this.TopMost = true;
+            worker.RunWorkerAsync();
+        }
+        
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+           
+            int y = Screen.PrimaryScreen.WorkingArea.Bottom - this.Height;
+            int x = Screen.PrimaryScreen.WorkingArea.Right - this.Width;
+            this.Location = new Point(x, y);
+            
+            envMonitor = new GlobalDataBase();
+            var connectionInfo = new ConnectionInfo("fireplug.dreamhost.com",
+                                                     "compusa",
+                                                     new PasswordAuthenticationMethod("compusa", "blackmagic1"));
+            compusaServer = new SftpClient(connectionInfo);
+            compusaServer.Connect();
+            if (compusaServer.IsConnected) {
+                envMonitor.ConnectionStatus = "compusa.live : connected";
+                
+                hireFisherMan();
+                
+        }
+        }
+        public void monitorSharedMemory()
+        {
+            string localDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) ;
+            string remoteDirectory = @"/home/compusa/compusa.live/php/sharedMemory";
+            
+            
+            List<SftpFile> availableFiles = compusaServer.ListDirectory(remoteDirectory).ToList();
+            foreach(var tempFile in availableFiles) {
+                string tempName = tempFile.Name;
+               
+                if ((tempName.EndsWith(".client"))){
+
+                    using(Stream fileStream = File.Create(localDirectory+"/"+tempName))
+                    {
+                        compusaServer.DownloadFile(remoteDirectory+"/"+tempName, fileStream);
+                    }
+                    string[] contents = File.ReadAllLines(localDirectory + "/" + tempName);
+                    foreach(string i in contents)
+                    {
+                        envMonitor.Log = i;
+                    }
+                    File.Delete(localDirectory + "/" + tempName);
+                    compusaServer.DeleteFile(remoteDirectory + "/" + tempName);
+                    //envMonitor.Log = tempName;
+                    envMonitor.NewContent = true;
+                }
+                
+                
+            }
+            
+            
+        }
+        private void updateGUI()
+        {
+            if(envMonitor != null)
+            {
+                txt_logger.Clear();
+                foreach(string message in envMonitor.Logger)
+                {
+                    txt_logger.AppendText(message + "\n");
+                    
+                }              
+            }
+        }
+        class GlobalDataBase
+        {
+            private bool newContent;
+            private string connectionStatus;
+            private List<string> logger = new List<string>();
+            private string log;
+            public string Log
+            {
+                get { return log; }
+                set {
+                    log = value;
+                    Logger.Add(value);
+                }
+            }
+
+            public List<string> Logger {
+                get { return logger; }
+                set { logger = value;  }
+                }
+            public string ConnectionStatus { get; set; }
+
+            public bool NewContent
+            {
+                get
+                {
+                    return newContent;
+                }
+
+                set
+                {
+                    newContent = value;
+                }
+            }
+
+            public GlobalDataBase()
+            {
+                connectionStatus = "compusa.live : disconnected";
+                logger = new List<string>();
+                
+            }
+            
+        }
+        //protected override void WndProc(ref Message m)
+        //{
+        //    switch (m.Msg)
+        //    {
+        //        case 0x84:
+        //            base.WndProc(ref m);
+        //            if ((int)m.Result == 0x1)
+        //                m.Result = (IntPtr)0x2;
+        //            return;
+        //    }
+
+        //    base.WndProc(ref m);
+        //}
+        //private void x_Click(object sender, EventArgs e)
+        //{
+        //    Application.Exit();
+        //}
+
+        
+    }
+}
